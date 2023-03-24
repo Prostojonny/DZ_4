a60ad90a9a40303b14186b501f8865e17c4a5b83
Alexey <prostojonny@beatspro.ru>
2023-03-24 19:26:08 +0400

    25, 27, 29

--- Program.cs
+++ Program.cs
@@ -1,31 +1,31 @@
 ﻿// 25 задача
 
-// Console.WriteLine("введите число A");
-// int a = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("введите число B");
-// int b = Convert.ToInt32(Console.ReadLine());
-// int step = a;
+Console.WriteLine("введите число A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+int step = a;
 
-// for (int i = 1; i < b; i++)
-// {
-// step = step * a;
-// }
-// Console.WriteLine("A в степени B равно: " + step);
+for (int i = 1; i < b; i++)
+{
+step = step * a;
+}
+Console.WriteLine("A в степени B равно: " + step);
 
 
 // 27 задача
 
-// Console.WriteLine("введите число");
-// int i = Convert.ToInt32(Console.ReadLine());
-// int sum = 0;
+Console.WriteLine("введите число");
+int i = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
 
-// while (i > 0)
-// {
-// int num = i % 10;
-// i = i / 10;
-// sum = sum + num;
-// }
-// Console.WriteLine("сумма всех цифр в числе равна: " + sum);
+while (i > 0)
+{
+int num = i % 10;
+i = i / 10;
+sum = sum + num;
+}
+Console.WriteLine("сумма всех цифр в числе равна: " + sum);
 
 // Задача 29
 
